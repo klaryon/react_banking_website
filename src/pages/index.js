ea949fc8ae0f76f89d5d18cec274632c29231307
klaryon <klary.ardon@gmail.com>
2021-03-07 18:24:13 +0100

    hero section styled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import HeroSection from '../components/HeroSection';
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
@@ -12,7 +13,8 @@ const toggle = () => {
     return (
         <>
          <Sidebar isOpen={isOpen} toggle={toggle}/>
-         <Navbar toggle={toggle}/>   
+         <Navbar toggle={toggle}/>
+         <HeroSection />
         </>
     )
 }
